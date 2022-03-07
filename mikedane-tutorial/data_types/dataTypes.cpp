854b67b96be7a56fc46a28456e30f9921727023e
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-03-07 22:12:24 +0000

    Data Types C++ file uploaded

---
+++ mikedane-tutorial/data_types/dataTypes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char grade = 'A';
+    string phrase  = "Giaffe Academy";
+    int age = 50;
+    double gpa = 2.3;
+    bool isMale = true;
+    bool isTall = false;
+
+    string name = "Mike";
+   
+    cout << name << " works for " << phrase << endl;
+    cout << isMale << endl;
+    cout << age << endl;
+
+    return 0;
+}
