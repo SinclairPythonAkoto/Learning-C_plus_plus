f8216f182091d18a484754c5d57fce98bfd8c0a9
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-03-16 01:33:51 +0000

    Visitor Login C++ file
    HR functionality to create a new staff member,
    staff saved into a CSV file;
    Next create functionality to remove staff.

--- visitor-login/visitorLogin.cpp
+++ visitor-login/visitorLogin.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 class VisitorLogin 
 {
@@ -7,6 +8,7 @@ class VisitorLogin
 
 	public:
 		void mainMenu();
+		void adminMenu();
 };
 
 void VisitorLogin :: mainMenu(){
@@ -29,30 +31,84 @@ menu:
 				   std::cin >> hrPass;
 
 				   if (hrPass == adminPass){
-					   std::cout << "HR LOGIN" << std::endl;
+					   adminMenu();
+					   break;
 				   }
 				   else{
 					   goto menu;
 				   }
 				   break;
 			   }
-			   case 2:{
+
+		case 2:{
 				   std::cout << "STAFF LOGIN" << std::endl; break;
 			   }
-			   case 3:{
+
+		case 3:{
 				   exit(0); break;
 			   }
+
 		default:
 			   std::cout << "Please select a valid number from the options listed." << std::endl;
 			   goto menu;
 	}
 }
 
+void VisitorLogin :: adminMenu(){
+menu:
+	int choice;
+system("clear");
+	std::cout << "Adminstration Menu" << std::endl;
+	std::cout << "\n\n 1) Create staff member" << std::endl;
+	std::cout << "\n\n 2) Remove staff member" << std::endl;
+	std::cout << "\n\n 3) Back to main menu" << std::endl;
+	std::cout << "\n\n 4) Exit" << std::endl;
+
+	std::cout << "\n\n Please select from the options above: ";
+	std::cin >> choice;
+
+	switch(choice){
+		case 1:{
+				   int staffId;
+				   std::string firstName, lastName, department, username, staffPass;
+
+				   system("clear");
+				   std::cout << "Create new staff member." << std::endl;
+				   std::cout << "Please fill the details to create a new employee.\n\n" << std::endl;
+				   std::cout << "Enter staff ID: ";
+				   std::cin >> staffId;
+				   std::cout << "\n\n Enter staff first name: ";
+				   std::cin >> firstName;
+				   std::cout << "\n\n Enter staff last name: ";
+				   std::cin >> lastName;
+				   std::cout << "\n\n Enter staff department: ";
+				   std::cin >> department;
+				   std::cout << "\n\n Enter staff username: ";
+				   std::cin >> username;
+				   std::cout << "\n\n Create staff password: ";
+				   std::cin >> staffPass;
+
+				   std::ofstream newStaff("companyEmployees.csv", std::ios::app|std::ios::out);
+				   newStaff << staffId << "," << firstName << "," << lastName << "," << department << "," << username << "," << staffPass;
+				   system("clear");
+				   std::cout << "\n Employee successfully created!" << std::endl;
+				   goto menu;
+				   break;
+			   }
+
+		default:
+			   std::cout << "Please select a correct option." << std::endl;
+			   goto menu;
+
+	}
+
+}
+
 int main()
 {
 	VisitorLogin visitorlogin;
 	visitorlogin.mainMenu();
-		//std::cout << "hello world!" << std::endl;
+	//std::cout << "hello world!" << std::endl;
 
 	return(0);
 }
