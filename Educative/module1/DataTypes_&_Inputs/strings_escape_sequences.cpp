1faf730c14e62444847dbf085dcdaa6c906cec03
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-09-30 14:44:23 +0100

    strings & escape sequences

---
+++ Educative/module1/DataTypes_&_Inputs/strings_escape_sequences.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string text = "Hello\nmy\name\nis\nSinclair";
+    cout << text ;
+}
+
+
+/*
+An escape sequence is used tp modify th e format of the output.
+
+Here is a list of common escape sequences
+
+- \n =  New Line        => Moves the cursor to the beginning of new line
+- \t =  Horzontal tab   => Moves the cursor 8x spaces to the right
+- \" =  Double qoutes   => Adds double qoutes inside the output
+- \\ =  Blackslah       => Displays a blackslash character
+- \' = Single qoute     => Inserts single qoute in the output
+- \b = Backspace        => deletes the last character
+
+*/
